91cad6367cb4f935621560984e41fd7f58d17d35
SwiftViktor <panafviktor@gmail.com>
2020-11-02 15:15:45 +0300

    Initial Commit

---
+++ MurmanskNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MurmanskNews
+//
+//  Created by Viktor on 02.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
