cf4fb7a7ae8118b9619baf5c5438fc4ab98f7886
SwiftViktor <panafviktor@gmail.com>
2020-11-04 00:29:22 +0300

    0.2v

--- MurmanskNews/PostTableViewCell.swift
+++ MurmanskNews/PostTableViewCell.swift
@@ -14,7 +14,7 @@ class PostTableViewCell: UITableViewCell {
     //First layer
     let cardView: UIView = {
         let view = UIView(frame: .zero)
-        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        view.backgroundColor = .clear
         view.layer.cornerRadius = 10
         view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -24,9 +24,19 @@ class PostTableViewCell: UITableViewCell {
     let firstStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
         stackView.axis = .vertical
-        stackView.distribution = .fillProportionally
+        stackView.distribution = .fillEqually
         stackView.alignment = .leading
-        stackView.spacing = 2
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    let secondStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -34,9 +44,9 @@ class PostTableViewCell: UITableViewCell {
     let ratingStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
         stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .leading
-        stackView.spacing = 2
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -44,14 +54,22 @@ class PostTableViewCell: UITableViewCell {
     let durationStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
         stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .leading
-        stackView.spacing = 2
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 0
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    
+    let thirdView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 10
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     let mainImageView: WebImageView = {
         let imageView = WebImageView()
         imageView.contentMode =  .scaleAspectFit
@@ -64,7 +82,7 @@ class PostTableViewCell: UITableViewCell {
         let label = UILabel()
         label.textColor =  #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
         label.font = UIFont.boldSystemFont(ofSize: 10)
-        label.textAlignment = .left
+        label.textAlignment = .right
         label.numberOfLines = 1
         return label
     }()
@@ -72,8 +90,8 @@ class PostTableViewCell: UITableViewCell {
     let titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        label.textAlignment = .natural
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textAlignment = .right
         label.numberOfLines = 2
         return label
     }()
@@ -87,41 +105,38 @@ class PostTableViewCell: UITableViewCell {
         return label
     }()
     
-    
-    let bottonView: UIView = {
-        let view = UIView(frame: .zero)
-        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        view.layer.cornerRadius = 10
-        view.clipsToBounds = true
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    
     let ratingImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "rating")
+        imageView.contentMode =  .scaleAspectFit
         return imageView
     }()
     
-    let ratingLabel: UITextView = {
-        let textView = UITextView()
-        
-        return textView
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .natural
+        label.numberOfLines = 2
+        return label
     }()
     
     let durationImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "duration")
+        imageView.contentMode =  .scaleAspectFit
         return imageView
     }()
     
-    let durationLabel: UITextView = {
-        let textView = UITextView()
-        
-        return textView
+    let durationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .natural
+        label.numberOfLines = 2
+        return label
     }()
     
     override func prepareForReuse() {
@@ -143,8 +158,8 @@ class PostTableViewCell: UITableViewCell {
         titleLabel.text = post.title
         descriptionLabel.text = post.description
         cityNameLabel.text = "Мурманск"
+        ratingLabel.text = "\(post.rating ?? 0.0)"
         durationLabel.text = post.duration
-        durationLabel.text = "\(post.rating ?? 0.0)"
     }
     
     private func setupFirstlayer() {
@@ -160,44 +175,51 @@ class PostTableViewCell: UITableViewCell {
         ])
         
         NSLayoutConstraint.activate([
-            mainImageView.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 1),
+            mainImageView.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 0),
             mainImageView.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
             mainImageView.widthAnchor.constraint(equalToConstant: screenSize.size.width * 0.98),
-            mainImageView.heightAnchor.constraint(equalToConstant: screenSize.size.height / 3.5)
+            mainImageView.heightAnchor.constraint(equalToConstant: screenSize.size.height / 4)
         ])
         
         NSLayoutConstraint.activate([
-            firstStackView.topAnchor.constraint(equalTo: mainImageView.bottomAnchor, constant: 5),
+            firstStackView.topAnchor.constraint(equalTo: mainImageView.bottomAnchor, constant: 0),
             firstStackView.centerXAnchor.constraint(equalTo: cardView.centerXAnchor),
             firstStackView.widthAnchor.constraint(equalToConstant: screenSize.size.width * 0.98),
-            firstStackView.heightAnchor.constraint(equalToConstant: screenSize.size.height / 4.5),
-            firstStackView.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -2),
+            firstStackView.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -5),
         ])
         
         firstStackView.addArrangedSubview(cityNameLabel)
         firstStackView.addArrangedSubview(titleLabel)
-        firstStackView.addArrangedSubview(bottonView)
+        firstStackView.addArrangedSubview(secondStackView)
         
         NSLayoutConstraint.activate([
-            bottonView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0),
-            bottonView.bottomAnchor.constraint(equalTo: firstStackView.bottomAnchor, constant: 0),
-            bottonView.leftAnchor.constraint(equalTo: firstStackView.leftAnchor, constant: 0),
-            bottonView.rightAnchor.constraint(equalTo: firstStackView.rightAnchor, constant: -1),
+            secondStackView.topAnchor.constraint(equalTo: firstStackView.bottomAnchor, constant: 5),
+            secondStackView.leftAnchor.constraint(equalTo: firstStackView.leftAnchor, constant: 5),
+            secondStackView.rightAnchor.constraint(equalTo: firstStackView.rightAnchor, constant: 5),
         ])
         
-        bottonView.addSubview(ratingStackView)
-        bottonView.addSubview(durationStackView)
+        secondStackView.addArrangedSubview(ratingStackView)
+        secondStackView.addArrangedSubview(durationStackView)
+        secondStackView.addArrangedSubview(thirdView)
+        
         ratingStackView.addArrangedSubview(ratingImage)
         ratingStackView.addArrangedSubview(ratingLabel)
+        
+        NSLayoutConstraint.activate([
+            ratingImage.leftAnchor.constraint(equalTo: ratingStackView.leftAnchor, constant: 5),
+            ratingImage.widthAnchor.constraint(equalToConstant: 20),
+            ratingImage.heightAnchor.constraint(equalToConstant: 20),
+            ratingLabel.leftAnchor.constraint(equalTo: ratingImage.rightAnchor, constant: -15),
+        ])
+        
         durationStackView.addArrangedSubview(durationImage)
         durationStackView.addArrangedSubview(durationLabel)
         
         NSLayoutConstraint.activate([
-            bottonView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0),
-            bottonView.bottomAnchor.constraint(equalTo: firstStackView.bottomAnchor, constant: 0),
-            bottonView.leftAnchor.constraint(equalTo: firstStackView.leftAnchor, constant: 0),
-            bottonView.rightAnchor.constraint(equalTo: firstStackView.rightAnchor, constant: -1),
+            durationImage.leftAnchor.constraint(equalTo: durationStackView.leftAnchor, constant: 0),
+            durationImage.widthAnchor.constraint(equalToConstant: 20),
+            durationImage.heightAnchor.constraint(equalToConstant: 20),
+            durationLabel.leftAnchor.constraint(equalTo: durationImage.rightAnchor, constant: -15),
         ])
-        
     }
 }
